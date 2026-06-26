3680fd3492a04a20ecd5c68628443f0d93c0bbca
esterm7 <ester.masala@accenture.com>
2026-06-26 12:37:24 +0200

    ngmodel camera-create

---
+++ src/app/DTO/cameraDTO.ts
@@ -0,0 +1,16 @@
+
+
+export class cameraDTO {
+   
+    postiLetto!: number;
+    postiLettoError!: boolean;
+    tipologia!: number;
+    tipologiaError!: boolean;
+    tariffa!: number;
+    tariffaError!: boolean;
+
+
+    constructor () {
+
+    }
+}
--- src/app/features/camera-create/camera-create.ts
+++ src/app/features/camera-create/camera-create.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { cameraDTO } from '../../DTO/cameraDTO';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-camera-create',
-  imports: [],
+  standalone:true,
+  imports: [FormsModule],
   templateUrl: './camera-create.html',
   styleUrl: './camera-create.css',
 })
-export class CameraCreate {}
+export class CameraCreate {
+
+  camera: cameraDTO;
+
+  //fare i controlli di validazione dei campi,
+  // se non sono validi non fare il submit del form, altrimenti fare il submit 
+  // e stampare in console i dati della camera da salvare
+
+  constructor (private router: Router) {
+    this.camera = new cameraDTO();
+  }
+
+  
+  submitForm() {
+    console.log ('Camera da salvare:', this.camera);
+    this.router.navigate(['/camera-create']);
+  }
+
+  resetForm() {
+    this.camera = new cameraDTO();
+  }
+}
+
+
+
+//creare dto, creare costruttore che istantzia il dto (cel componente es camera-create), 
