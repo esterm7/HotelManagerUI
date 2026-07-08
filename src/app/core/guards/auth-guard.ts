94ebdd46fb8fd0bf861341e8cb37db83a2e0f852
esterm7 <ester.masala@accenture.com>
2026-07-08 12:27:49 +0200

    auth-guard e s interceptor

---
+++ src/app/core/guards/auth-guard.ts
@@ -0,0 +1,17 @@
+
+import {inject} from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/AuthService';
+
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()){
+  return true;
+}
+
+router.navigate(['/utente-login']);
+return false;
+
+};
