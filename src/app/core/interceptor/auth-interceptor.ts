2b4f98640310275db38fef381c6643e6c4c6bd38
SimoneDessi01 <simone.dessi@accenture.com>
2026-07-09 10:32:02 +0200

    Generic error alert

--- src/app/core/interceptor/auth-interceptor.ts
+++ src/app/core/interceptor/auth-interceptor.ts
@@ -1,20 +1,47 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 
-import { inject} from '@angular/core';
+import { inject } from '@angular/core';
 
 import { AuthService } from '../../services/AuthService';
+import { AlertService } from '../../services/AlertService';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject (AuthService);
+  const authService = inject(AuthService);
+  const alertService = inject(AlertService);
+
+  const router = inject(Router);
+
   const token = authService.getToken();
 
 
-  if (token) {
-    const clonedReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
-    });
 
-    return next (clonedReq);
-  }
-  return next(req);
-};
+
+  const authReq = token
+    ? req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    : req;
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      if (error.status === 401) {
+        alertService.show('Sessione scaduta. Effettua nuovamente il login.');
+
+        authService.logout();
+
+        router.navigate(['/login']);
+      }
+
+      if (error.status === 403) {
+        alertService.show('Non hai i permessi per eseguire questa operazione.');
+      }
+
+      return throwError(() => error);
+    })
+  );
+}
