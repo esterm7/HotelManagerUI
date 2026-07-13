0bab3ce45fc0af3b4dc80188a54788625cc22bd5
SimoneDessi01 <simone.dessi@accenture.com>
2026-07-13 10:46:33 +0200

    modifica per evitare l'errore ReferenceError in alert

--- src/app/core/services/AlertService.ts
+++ src/app/core/services/AlertService.ts
@@ -1,12 +1,17 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
 
+  private platformId = inject(PLATFORM_ID);
+
   show(message: string) {
-    alert(message);
+    if (isPlatformBrowser(this.platformId)) {
+      alert(message);
+    }
   }
 
 }
