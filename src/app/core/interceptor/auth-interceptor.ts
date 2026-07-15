4a9bde42339b44dc6bff0ecb976c97991024070e
SimoneDessi01 <simone.dessi@accenture.com>
2026-07-15 11:09:41 +0200

    correzione di visualizzazione errore e reindirizzamento in home in caso di mancanza di permessi per entrare in una pagina

--- src/app/core/interceptor/auth-interceptor.ts
+++ src/app/core/interceptor/auth-interceptor.ts
@@ -40,12 +40,14 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       }
 
       if (error.status === 403 && !skipAlert) {
+        console.log('Non hai i permessi per eseguire questa operazione.');
         alertService.show('Non hai i permessi per eseguire questa operazione.');
         router.navigate(['/home']);
 
       }
 
       if (error.status === 500 && !skipAlert) {
+        console.log('Pagina non raggiungibile');
         alertService.show('Pagina non raggiungibile');
         router.navigate(['/home']);
 
