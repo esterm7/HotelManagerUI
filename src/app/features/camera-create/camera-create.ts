d23b62840c649d4547a0ff354d1329d1002614a7
esterm7 <ester.masala@accenture.com>
2026-07-17 12:19:23 +0200

    edit camera-create.ts

--- src/app/features/camera-create/camera-create.ts
+++ src/app/features/camera-create/camera-create.ts
@@ -30,9 +30,6 @@ export class CameraCreate implements OnInit {
   
   tipologie = Object.values(TipoCamera);
 
-
-
-  tipologie = Object.values(TipoCamera); 
     tipologiaSelezionata: TipoCamera | null = null;
   
 
