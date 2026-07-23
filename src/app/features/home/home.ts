a25b048346f18216ba4ffb2d140d308f4028981c
SimoneDessi01 <simone.dessi@accenture.com>
2026-07-23 09:50:37 +0200

    [Feat] modifiche ad home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,25 +1,69 @@
-import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/AuthService';
 import { Service } from '../../core/services/service';
 import { NavLayout } from '../../nav-layout/nav-layout';
+import { FormError } from '../form-error/form-error';
+import { PrenotazioneDTO } from '../../DTO/prenotazioneDTO';
+import { TipoCamera } from '../../core/enums/tipologia-camera-enum';
+import { FormsModule } from '@angular/forms';
+import { CameraDto } from '../../DTO/cameraDTO';
+import { CamereLibere } from '../camere-libere/camere-libere';
+import { Dialog } from '@angular/cdk/dialog';
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, NavLayout],
+  imports: [CommonModule, NavLayout, FormError, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 
 export class Home implements OnInit {
+  private dialog = inject(Dialog);
 
+  today = new Date().toISOString().split('T')[0];
+  tipologie = Object.values(TipoCamera);
 
-  constructor(private router: Router, private route: ActivatedRoute, public auth: AuthService, private service: Service) { }
+  prenotazioneDTO!: PrenotazioneDTO;
 
+  constructor(private router: Router, private route: ActivatedRoute, public auth: AuthService, private service: Service) {
+    this.prenotazioneDTO = new PrenotazioneDTO();
+    this.prenotazioneDTO.dataPrenotazione = new Date();
+    this.prenotazioneDTO.tipologiaCamera = null;
+  }
 
   livelloPermesso!: string | null;
+
+
+  vaiAPrenota() {
+    this.router.navigate(['/prenotazione-create'], {
+      state: { prenotazioneTemp: this.prenotazioneDTO }
+    });
+  }
+
+  apriSelezioneCamera() {
+    this.prenotazioneDTO.inputValidate();
+    if (this.prenotazioneDTO.dataInizioError || this.prenotazioneDTO.dataFineError || this.prenotazioneDTO.tipologiaCameraError) {
+      alert('Campi necessari per la selezione della camera non validi');
+      return;
+    }
+    const ref = this.dialog.open<CameraDto | null>(CamereLibere, {
+      data: {
+        prenotazioneTemp: this.prenotazioneDTO,
+      }
+    });
+
+    ref.closed.subscribe(result => {
+      if (result) {
+        console.log('Camera scelta:', result.codiceCamera);
+        this.prenotazioneDTO.codiceCamera = result.codiceCamera;
+      }
+      this.vaiAPrenota();
+      console.log('Login chiuso, risultato:', result);
+    });
+  }
 
 
   vaiAlCreaUtente() {
